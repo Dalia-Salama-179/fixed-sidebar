8148582b896df5aad7b53f2866508fde4f389828
unknown <daliasalama179@gmail.com>
2020-01-08 21:12:10 +0200

    fixed column on scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(window).on('load', function () {
+    let $fixedCol = $('.d-fixed-col');
+    let $scrolledCol = $('.d-scrolled-col');
+    let fixedColHeight = $fixedCol.outerHeight();
+    let fixedColWidth = $fixedCol.outerWidth();
+    let scrolledColHeight = $scrolledCol.outerHeight();
+    let offsetTop = $scrolledCol.offset().top;
+    let windowScrollTop,
+        windowHeight = $(window).height();
 
+
+    if (scrolledColHeight > fixedColHeight) {
+        $fixedCol.css('width', fixedColWidth)
+        $(window).scroll(function () {
+            windowScrollTop = $(window).scrollTop();
+            if (fixedColHeight - offsetTop < windowHeight) {
+                if (windowScrollTop >= offsetTop) {
+                    $fixedCol.addClass('fixed-on-scroll')
+                } else {
+                    $fixedCol.removeClass('fixed-on-scroll')
+                }
+            }
+        })
+    }
 });
